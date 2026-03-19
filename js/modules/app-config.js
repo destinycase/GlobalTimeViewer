cb48cbdc47df7dd2fe36c5bdb87a99ebe2dc238d
destinycase <xellose10@gmail.com>
2026-03-20 01:21:26 +0900

    3.11.3

--- js/modules/app-config.js
+++ js/modules/app-config.js
@@ -2,7 +2,7 @@
     "use strict";
 
     const APP_CONFIG = Object.freeze({
-        VERSION: "3.11.2",
+        VERSION: "3.11.3",
         STORAGE_KEY: "GTV_v323_Data",
         THEME_STORAGE_KEY: "GTV_Theme",
         LANG_STORAGE_KEY: "GTV_Lang",
